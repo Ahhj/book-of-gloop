50c4799eb8efcfd0d3d8be5fefa9a08ad62f04e3
KatrionaGoldmann <k.goldmann@qmul.ac.uk>
2020-04-15 22:05:26 +0100

    tabletop version

---
+++ kg-recipe/src/components/Contents.js
@@ -0,0 +1,55 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Page from './Page'
+import Recipe from './Recipe'
+import Home from './Home'
+import './Contents.css'
+
+export default function Contents(props) {
+  const links = props.recipes.map(
+    (recipe) =>
+      <li>
+        <Link
+          to={`/${recipe.name}`}
+          style={{ textDecoration: 'none' }}
+        >
+          {recipe.name}
+        </Link>
+      </li>
+  );
+
+  var routes = props.recipes.map(
+    (recipe) =>
+      <Route exact path={`/${recipe.name}`}>
+        <Recipe
+          name={recipe.name}
+          ingredients={recipe.ingredients}
+          instructions={recipe.instructions}
+        />
+      </Route>
+  );
+  // Append home and contents to routes.
+  routes = [
+    <Route exact path={"/"}><Home /></Route>,
+    <Route exact path={"/Contents"}><Page name={"Contents"} content={<ol>{links}</ol>}/></Route>
+  ].concat(routes)
+  return (
+    <Router>
+      <div>
+        {/*
+          A <Switch> looks through all its children <Route>
+          elements and renders the first one whose path
+          matches the current URL. Use a <Switch> any time
+          you have multiple routes, but you want only one
+          of them to render at a time
+        */}
+        <Switch>{routes}</Switch>
+      </div>
+    </Router>
+  );
+}
---
+++ kg-recipe/src/components/Home.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {
+  Route,
+  Link
+} from "react-router-dom";
+import Page from './Page';
+import './Home.css';
+import baking from './images/baking.jpg';
+import christmas from './images/christmas.jpg';
+import clams from './images/clams.jpg';
+import cocktail from './images/cocktail.jpg';
+import dfo from './images/dfo.jpg';
+import kitchen from './images/kitchen.jpg';
+
+export default function Home() {
+  return (
+    <div className="Home">
+      <div className='row'>
+        <div className='col'><img src={baking} /></div>
+        <div className='col'><img src={christmas} /></div>
+        <div className='col'><img src={clams} /></div>
+      </div>
+      <Page name={Home} content={<h1>The Gloop Collection</h1>} />
+      <div className='row'>
+        <div className='col'><img src={cocktail} /></div>
+        <div className='col'><img src={dfo} /></div>
+        <div className='col'><img src={kitchen} /></div>
+      </div>
+      <Route>
+        <Link to={'/Contents'}>{'Go to contents'}</Link>
+      </Route>
+    </div>
+  );
+}
