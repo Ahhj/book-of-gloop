674b5b0011ed0940d8b6871a6725564b58f09932
Alistair Jones <alistair.hh.jones@gmail.com>
2020-12-03 16:37:55 +0000

    Fix string joining

--- client/src/components/RecipeContainer/index.jsx
+++ client/src/components/RecipeContainer/index.jsx
@@ -37,15 +37,15 @@ export default function RecipeContainer() {
         // Handle pre-markdown files
         if (key === "ingredients") {
           if (Array.isArray(data[key])) {
-            value = data[key].map((item) =>
-              `- ${item.quantity} ${item.name}`.join("\n")
-            );
+            value = data[key]
+              .map((item) => `- ${item.quantity} ${item.name}`)
+              .join("\n");
           }
         } else if (key === "steps") {
           if (Array.isArray(data[key])) {
-            value = data[key].map((item) =>
-              `1. ${item.description}`.join("\n")
-            );
+            value = data[key]
+              .map((item) => `1. ${item.description}`)
+              .join("\n");
           }
         }
 
